1a239d9101e9ffca78669487606d9d30dd00a754
Rm Carting <Rm Carting@aspire7>
2024-10-01 19:24:13 +0800

    Version 1

--- ACTIVITY/src/activity/ACTIVITY_RACHO_CARTING.java
+++ ACTIVITY/src/activity/ACTIVITY_RACHO_CARTING.java
@@ -1,14 +1,27 @@
 
 package activity;
 
+import java.util.Scanner;
+
 
 public class ACTIVITY_RACHO_CARTING {
 
    
     public static void main(String[] args) {
+
+      
+        Scanner scn = new Scanner (System.in);
         
+         System.out.println("Enter the radius of the circle:");
+         
+        double radius = scn.nextDouble();
+        
+        double area= Math.PI * radius * radius;
+        
+        System.out.println("The area of the circle is:"+ area);
+    }
         
         
     }
     
-}
+
